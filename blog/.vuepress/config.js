ec3038df5139685f1f8ee8d5f57c7fe30c5a9f45
nexta1 <798868370@qq.com>
2023-05-23 01:59:58 +0800

    add

--- blog/.vuepress/config.js
+++ blog/.vuepress/config.js
@@ -1,7 +1,21 @@
 module.exports = {
   title: "知识",
   description: "Just playing around",
-  plugins: ["@vuepress/blog"],
+  plugins: [
+    [
+      "@vuepress/blog",
+      {
+        directories: [
+          {
+            id: "post",
+            dirname: "_posts",
+          },
+        ],
+      },
+    ],
+    "@vuepress/back-to-top",
+  ],
+
   themeConfig: {
     lastUpdated: "Last Updated", // string | boolean
     displayAllHeaders: true, // 默认值：false
@@ -15,7 +29,7 @@ module.exports = {
           {
             text: "常用工具",
             items: [
-              { text: "Markdown", link: "/tool/markdown" },
+              { text: "Markdown", link: "/tool/markdown/" },
               { text: "Git", link: "/tool/git/" },
               { text: "Brew", link: "/tool/brew/" },
             ],
@@ -23,8 +37,8 @@ module.exports = {
           {
             text: "Node tools",
             items: [
-              { text: "npm", link: "/tool/markdown" },
-              { text: "yarn", link: "/tool/git/" },
+              { text: "npm", link: "/tool/node/" },
+              { text: "yarn", link: "/tool/node/" },
             ],
           },
         ],
@@ -39,7 +53,6 @@ module.exports = {
       },
       {
         text: "Spring",
-        ariaLabel: "Language Menu",
         items: [
           {
             text: "基础知识",
@@ -80,6 +93,31 @@ module.exports = {
         items: [
           { text: "Mysql", link: "/db/mysql.md" },
           { text: "Redis", link: "/db/redis.md" },
+        ],
+      },
+      {
+        text: "External",
+        items: [
+          {
+            text: "Vue",
+            items: [
+              {
+                text: "VuePress",
+                link: "https://vuepress.vuejs.org/zh/guide/",
+              },
+              {
+                text: "Nuxt",
+                link: "https://nuxt.org.cn/getting-started/introduction",
+              },
+            ],
+          },
+          {
+            text: "Spring",
+            items: [
+              { text: "JPA", link: "/spring/springDataJpa.md" },
+              { text: "Security", link: "/spring/SpringSecurity.md" },
+            ],
+          },
         ],
       },
       { text: "ChatGPT", link: "https://ai.com" },
---
+++ blog/README.md
@@ -0,0 +1,16 @@
+---
+home: true
+heroText: 文档知识
+tagline: 决心决定行动，行动决定成就。
+actionText: 快速提高 →
+actionLink: /spring/springDataJpa.md
+features:
+  - title: 记录知识
+    details: 以 Markdown 为中心的项目结构，以最少的配置帮助你专注于写作。
+  - title: 全栈能力
+    details: 全栈工程师拥有对于前端、后端以及数据库等多个技术领域的深入理解和掌握，能够从项目的设计、开发到部署和维护等各个环节进行全面的工作。
+  - title: 持续提高
+    details: 每一次努力和进步都是你迈向成功的一小步。不要畏惧挑战，因为挑战是你成长的机会。相信自己的能力，坚持不懈地追求进步，你将超越自己的极限，达到更高的高度。
+footer: MIT Licensed | Copyright © 2023-present Next1 Zhou
+---
+
